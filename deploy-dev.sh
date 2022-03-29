014dd60bf4e6b60bd32ec80ee2abe1fc1560ea64
liuyav <2472479696@qq.com>
2022-03-30 00:52:22 +0800

    mod

--- deploy-dev.sh
+++ deploy-dev.sh
@@ -4,4 +4,7 @@ echo Deploy Project ...
 git pull origin dev
 
 # 设置淘宝源安装依赖，之后打包
-npm config set registry http://registry.npm.taobao.org/ && npm i && npm run dev
+npm config set registry http://registry.npm.taobao.org/ && npm i && npm run build
+
+
+docker run -p 8000:80 -v $PWD/www:/usr/share/nginx/html -d nginx
